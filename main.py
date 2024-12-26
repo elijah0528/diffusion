d9ae12950561480b0109b431ec71c4a7d163904d
Elijah Kurien <elijahkurien05@gmail.com>
2024-12-26 18:12:30 -0500

    [Add] Diffusion is hard

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from datasets import load_dataset
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,9 +16,10 @@ from PIL import Image
 
 # Constants
 batch_size = 4
-resized_size = 64
+resized_size = 128
 train_ratio = 0.8
-timesteps = 100001
+timesteps = 50000
+dim_embeddings = 32 # Change to 32
 
 # Define data transform
 transform = transforms.Compose([
@@ -71,19 +73,19 @@ train_dataset, test_dataset = random_split(face_dataset, [train_size, test_size]
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
+# Returns index for batches to
+def get_index_from_list(vals, t, x_shape):
+    # x_shape of size (B, C, H, W)
+    out = vals.gather(-1, t)
+    return out.reshape(batch_size, *((1, )  * (len(x_shape) - 1)))# new batch_x of size (B, 1, H, W)
 
 
-def get_index_from_list(batch_x, t):
-    if len(batch_x.shape) == 3:
-        batch_x.unsqueeze(0)
-    print(batch_x.shape)
-    return batch_x[:, t, :, :]
-
+# Gets diffused sample at an arbitrary timestep
 def forward_sample(x_0, t, device='cpu'):
     noise = torch.randn_like(x_0)
-    sqrt_one_minus_alphas_cumprod_t = sqrt_one_minus_alphas_cumprod[t]
-    alpha_sqrt_cumprod_t = alpha_sqrt_cumprod[t]
-    return alpha_sqrt_cumprod_t * x_0 + sqrt_one_minus_alphas_cumprod_t * noise
+    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(sqrt_one_minus_alphas_cumprod, t, x_0.shape)
+    alpha_sqrt_cumprod_t = get_index_from_list(alpha_sqrt_cumprod, t, x_0.shape)
+    return alpha_sqrt_cumprod_t * x_0 + sqrt_one_minus_alphas_cumprod_t * noise, noise
 
 
 # Betas is [steps]
@@ -99,54 +101,121 @@ posterior_variance = betas * (1. - alpha_cumprod_prev) / (1. - alpha_cumprod)
 
 print(betas.shape)
 
+
+class PositionalEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+    def forward(self, t):
+        half_dim = self.dim // 2
+        print(t)
+        b = (torch.arange(100000) / 10000.).unsqueeze(1) # (pos_len, 1)
+        e = torch.arange(half_dim) / self.dim
+        e = e.unsqueeze(0) # (1, half_dim)
+        embeddings = b ** e # (pos_len, half_dim)
+        embeddings = torch.stack((embeddings.sin(), embeddings.cos()), dim=-1).flatten(start_dim=-2)
+        return embeddings
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
+        super().__init__()
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.time_mlp =  nn.Linear(time_emb_dim, out_ch)
+        self.up = up
+
+        if up:
+            self.conv1 = nn.Conv2d(2 * self.in_ch, self.out_ch, 3, padding=1)
+            self.transform = nn.ConvTranspose2d(self.out_ch, self.out_ch, 4, 2, 1)
+        else:
+            self.conv1 = nn.Conv2d(self.in_ch, self.out_ch, 3, padding=1)
+            self.transform = nn.Conv2d(self.out_ch, self.out_ch, 4, 2, 1)
+
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(out_ch)
+        self.bnorm2 = nn.BatchNorm2d(out_ch)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x, t):
+        h = self.bnorm1(self.relu(self.conv1(x)))
+
+        time_emb = self.relu(self.time_mlp(t)) # Time embedding is of shape (out_ch)
+        print(f"Expected shape of time_emb is (out_ch): {time_emb.shape}")
+        time_emb = time_emb[(...,) + (None,) * 2]
+        print(f"Expected shape of time_emb is (out_ch [1] [1]): {time_emb.shape}")
+        print(f"H shape: {h.shape}")
+        print(f"time emb shape: {time_emb.shape}")
+        h = h + time_emb
+
+        h = self.bnorm2(self.relu(self.conv2(h)))
+
+        return self.transform(h)
+    
+
 class SimpleUNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.image_channels = 3
-        self.down_channels = (64, 128, 256, 512)
-        self.up_channels = (512, 256, 128, 64)
+        self.down_channels = (64, 128, 256, 512, 1024)
+        self.up_channels = (1024, 512, 256, 128, 64)
         self.out_dim = 3
         self.time_emb_dim = 32
 
+        self.time_mlp = nn.Sequential(
+            PositionalEmbeddings(dim_embeddings),
+            nn.Linear(dim_embeddings, dim_embeddings),
+            nn.ReLU(),
+        )
 
+        self.conv0 = nn.Conv2d(self.image_channels, self.down_channels[0], 3, padding=1)
+        
+        self.downs = nn.ModuleList([
+            Block(self.down_channels[i], self.down_channels[i + 1], self.time_emb_dim) for i in range(len(self.down_channels) - 1)
+        ])
+        self.ups = nn.ModuleList([
+            Block(self.up_channels[i], self.up_channels[i + 1], self.time_emb_dim, up=True) for i in range(len(self.up_channels) - 1)
+        ])
 
+        self.output = nn.Conv2d(self.up_channels[-1], self.out_dim, 1)
 
-    def forward(self):
-        pass
+    def forward(self, x, timestep):
+        t = self.time_mlp(timestep) # (dim_embeddings)
+        
+        x = self.conv0(x)
 
+        residuals = []
+        for down in self.downs:
+            x = down(x, t)
+            residuals.append(x)
 
+        for up in self.ups:
+            residual_x = residuals.pop()
+            x = torch.cat((x, residual_x), dim=1)
+            x = up(x, t)
 
+        
+        return self.output(x)
 
+model = SimpleUNet()
+
+def get_loss(model, x_0, t):
+    x_noisy, noise = forward_sample(x_0, t)
+    noise_pred = model(x_noisy, t)
+    print(f"Noise shape{noise.shape}")
+    print(f"Predicated noise shape{noise_pred.shape}")
+
+    return F.l1_loss(noise, noise_pred)
 
 
 noised_images = []
-
 for batch in train_loader:
     print(batch.shape)
-    for img in batch:
-        print(img.max())
-        noised_images.append(forward_sample(img, 0))
-        noised_images.append(forward_sample(img, 100))
-        noised_images.append(forward_sample(img, 500))
-        noised_images.append(forward_sample(img, 1000))
-        noised_images.append(forward_sample(img, 50000))
+    t = torch.randint(0, timesteps, (batch_size, )).long()
+    loss = get_loss(model, batch, t)
+    print(loss)
+    break
 
-        break
-
-    '''
-    num_images = min(5, len(noised_images))
-    rows, cols = 1,5
-    plt.figure(figsize=(2 * cols, 2 * rows))
-    plt.axis('off')
-
-    for i in range(num_images):
-        plt.subplot(rows, cols, i + 1)
-        plt.title(f"iteration {i + 1}")
-        show_tensor_image(noised_images[i])
-
-    plt.tight_layout(pad=2.0)
-    plt.show()
-    '''
     # Pytorch images are C x H x W
     # Numpy images are H x W x C
 
